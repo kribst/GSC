e06d99cea18abfa26333989e014bfc0174a4609b
kribst <ilassidjoba@gmail.com>
2025-03-28 14:06:29 +0100

    first commit

--- src/Raport_de_Performance/settings.py
+++ src/Raport_de_Performance/settings.py
@@ -161,6 +161,6 @@ LOGOUT_REDIRECT_URL = "login"
 
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR, 'mediafiles/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles/')
